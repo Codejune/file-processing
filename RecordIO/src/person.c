fb3378de760b6c94eb242e8558e7b846f4f6b528
codejune <kbj9704@gmail.com>
2020-05-20 03:07:56 +0900

    bug....

--- RecordIO/src/person.c
+++ RecordIO/src/person.c
@@ -18,15 +18,6 @@ struct Header
 };
 
 /**
- * @brief Delete information structure
- */
-struct DeleteInfo
-{
-	int page;
-	int record;
-};
-
-/**
  * @brief Read page from record file
  * @param *char $pagebuf Array of char to store the read buffer
  * @param int $pagenum Page number to read
@@ -95,27 +86,32 @@ void insert(FILE *fp, const Person *p)
 	char pagebuf[PAGE_SIZE];
 	char recordbuf[RECORD_SIZE];
 	struct Header header;
-	struct DeleteInfo d_info;
+	int previous_delete_page;
+	int previous_delete_record;
 	int record_idx;
 
 	// Read header data
-	fseek(fp, 0, SEEK_SET);
-	fread(&header, sizeof(struct Header), 1, fp);
+	readPage(fp, pagebuf, 0);
+	memcpy(&header, pagebuf, sizeof(struct Header));
 
 	pack(recordbuf, p); // Packing input data to record buffer
 
 	if ((header.current_delete_page != -1) && (header.current_delete_record != -1)) { // Delete record is exist
 
-		fseek(fp, header.current_delete_page * PAGE_SIZE + header.current_delete_record * RECORD_SIZE + sizeof(char), SEEK_SET);
-		fread(&d_info, sizeof(struct DeleteInfo), 1, fp);
-
 		readPage(fp, pagebuf, header.current_delete_page);
+
+		// Parse previous delete page number and record number
+		memcpy(&previous_delete_page, pagebuf + sizeof(char), sizeof(int));
+		memcpy(&previous_delete_record, pagebuf + sizeof(char) + sizeof(int), sizeof(int));
+
+		// Write data to pagebuf
 		memset(pagebuf + header.current_delete_record * RECORD_SIZE, (char)0xFF, RECORD_SIZE);
 		strncpy(pagebuf + header.current_delete_record * RECORD_SIZE, recordbuf, strlen(recordbuf));
 		writePage(fp, pagebuf, header.current_delete_page);
 
-		header.current_delete_page = d_info.page;
-		header.current_delete_record = d_info.record;
+		// Header data update
+		header.current_delete_page = previous_delete_page;
+		header.current_delete_record = previous_delete_record;
 
 	} else { // Delete record doesn't exist
 
@@ -125,8 +121,6 @@ void insert(FILE *fp, const Person *p)
 
 		if (record_idx == 0) { // Page fully
 
-			printf("full!\n");
-
 			// Create new page and write page to record file
 			strncpy(pagebuf, recordbuf, strlen(recordbuf));
 			memset(pagebuf + RECORD_SIZE, (char)0xFF, PAGE_SIZE - RECORD_SIZE);
@@ -134,7 +128,6 @@ void insert(FILE *fp, const Person *p)
 
 		} else { // Page doesn't fully
 
-			printf("doesn't full!\n");
 			readPage(fp, pagebuf, header.page_count - 1);
 			memset(pagebuf + record_idx * RECORD_SIZE, (char)0xFF, RECORD_SIZE);
 			strncpy(pagebuf + record_idx * RECORD_SIZE, recordbuf, strlen(recordbuf));
@@ -160,42 +153,42 @@ void delete(FILE *fp, const char *sn)
 {
 	Person p;
 	struct Header header;
-	struct DeleteInfo d_info;
 	char pagebuf[PAGE_SIZE];
 	char recordbuf[RECORD_SIZE];
-	int page_count;
 	char symbol = '*';
 
 	// Read header data
-	fseek(fp, 0, SEEK_SET);
-	fread(&header, sizeof(struct Header), 1, fp);
+	readPage(fp, pagebuf, 0);
+	memcpy(&header, pagebuf, sizeof(struct Header));
 
-	// Get page count
-	fseek(fp, 0, SEEK_END);
-	page_count = ftell(fp) / PAGE_SIZE;
-
-	for (int i = 1; i <= page_count; i++) { // Page index loop
+	for (int i = 1; i < header.page_count; i++) { // Page index loop
 
 		readPage(fp, pagebuf, i); // Read page and copy content to page buffer
 
 		for (int j = 0; j < RECORD_PER_PAGE; j++) { // Record index loop
 
 			strncpy(recordbuf, pagebuf + j * RECORD_SIZE, RECORD_SIZE); // Read page buffer and copy content to record buffer
+
+			if(recordbuf[0] == '*')
+				continue;
+
 			unpack(recordbuf, &p); // Convert record buffer content to structure
 
 			if (strcmp(p.sn, sn) == 0) { // Find PERSON_ID
 
-				fseek(fp, i * PAGE_SIZE + j * RECORD_SIZE, SEEK_SET);
-				fwrite(&symbol, sizeof(char), 1, fp);
-				fwrite(&header.current_delete_page, sizeof(int), 1, fp);
-				fwrite(&header.current_delete_record, sizeof(int), 1, fp);
+				// Write previous delete page and record number to record buffer
+				memcpy(recordbuf, &symbol, sizeof(char)); // Write symbol to record buffer     
+				memcpy(recordbuf + sizeof(char), &header.current_delete_page, sizeof(int)); // Write previous delete page number to record buffer
+				memcpy(recordbuf + sizeof(char) + sizeof(int), &header.current_delete_record, sizeof(int)); // Write previous delete record number to record buffer
+				strncpy(pagebuf + j * RECORD_SIZE, recordbuf, RECORD_SIZE); // Write record buffer to page buffer
+				writePage(fp, pagebuf, i); // Write page buffer to record file
 
 				// Refresh header and delete info structure
 				header.record_count--;
 				header.current_delete_page = i;
 				header.current_delete_record = j;
 
-				// Refrest header data to record file
+				// Refresh header data to record file 
 				memset(pagebuf, (char)0xFF, PAGE_SIZE);
 				memcpy(pagebuf, &header, sizeof(struct Header)); // Convert header data to binary integer and write to page buffer
 				writePage(fp, pagebuf, 0); // Write page buffer in record file
